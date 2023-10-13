a545ce2718f13ac9dc9d3b70280061e974b71669
OwenRD <owenruizdiaz2004@gmail.com>
2023-10-12 22:00:38 -0300

    Agrgado de elementos

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Profile } from './pages/Perfil/Profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Profile/>
   </React.StrictMode>,
 )
--- FrontEnd/src/pages/Perfil/Profile.jsx
+++ FrontEnd/src/pages/Perfil/Profile.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export const Profile = () => {
-  return <div>Profile</div>;
+    return <>
+
+        <div className="conteiner">
+            <nav className="bg-blue-700 py-6 relative">
+                <div className="c-bar mx-auto flex flex-grow justify-between px-5"></div>
+            </nav>
+
+            <div className="h-screen m-10 flex">
+                <div className="bg-gray-300 h-screen w-2/5 rounded-3xl place-items-center">
+                    <div className="bg-gray-400 h-72 w-72 rounded-full"></div>
+                    <h1>User name</h1>
+                </div>
+
+                <div className="h-screen w-1 bg-gray-300 mx-5 rounded-md"></div>
+
+                <div className="bg-gray-300 h-screen w-3/5 rounded-3xl"></div>
+
+            </div>
+
+        </div>
+
+    </>;
 };
