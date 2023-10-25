ac1a31d1f3ff3cf85b8bb50238c44588d6b48dd2
OwenRD <owenruizdiaz2004@gmail.com>
2023-10-25 10:54:53 -0300

    Agregela ventana de creacion de proyecto

--- FrontEnd/src/components/Task.jsx
+++ FrontEnd/src/components/Task.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Proyect() {
+export default function Task() {
     return (
         <>
 
@@ -14,7 +14,11 @@ export default function Proyect() {
                 <div className='flex items-center'>
                     <p>Work in progress by</p>
                     <div className='h-12 w-12 rounded-full bg-gray-800 mx-5'></div>
-                    <div className='h-8 w-10 bg-red-600 rounded-lg'></div>
+                    <div className='bg-red-600 p-2 m-1 rounded-md cursor-pointer'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                    </svg>
+                    </div>
                 </div>
             </div>
 
--- FrontEnd/src/pages/LugarDeTrabajo/WorkPlace.jsx
+++ FrontEnd/src/pages/LugarDeTrabajo/WorkPlace.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Button from "../../components/Button";
-import Proyect from "../../components/Proyect";
+import Task from "../../components/Task";
 import Member from "../../components/Member";
+import ProyectForm from '../../components/ProyectForm';
 
 export const WorkPlace = () => {
 
@@ -10,60 +11,73 @@ export const WorkPlace = () => {
     const handleButtonClick = (buttonId) => {
         setActiveButton(buttonId);
     }
+    /* Codigo para el Modal */
+    const [visible, setVisible] = useState(false);
+    
 
     return <>
 
-            <div className="grid grid-cols-5 grid-rows-3 h-screen gap-5">
-                <div className="bg-gray-300 row-span-3 col-span-1 p-5 rounded-tr-3xl rounded-br-3xl">
+        <div className="grid grid-cols-5 grid-rows-3 h-screen gap-5">
+            <div className="bg-gray-300 row-span-3 col-span-1 p-5 rounded-tr-3xl rounded-br-3xl">
+                <div className="flex justify-between items-center">
                     <h1>Proyectos</h1>
-                    <div className="h-1 w-full bg-gray-400 rounded-2xl my-1"></div>
-
-                    <Button isActive={
-                            activeButton === 1
-                        }
-                        onClick={
-                            () => handleButtonClick(1)
-                        }/>
-                    <Button isActive={
-                            activeButton === 2
-                        }
-                        onClick={
-                            () => handleButtonClick(2)
-                        }/>
-                    <Button isActive={
-                            activeButton === 3
-                        }
-                        onClick={
-                            () => handleButtonClick(3)
-                        }/>
-
-                </div>
-
-                <div className="bg-gray-300 row-span-3 col-span-3 p-5">
-                    <div className="h-20 w-auto rounded-3xl bg-gray-500 text-center grid content-center">
-                        <h1 className="text-3xl">Proyecto X</h1>
+                    <div className="bg-blue-600 p-2 m-1 rounded-md cursor-pointer" 
+                    onClick={() => setVisible(!visible)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-plus-fill" viewBox="0 0 16 16">
+                            <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0z"/>
+                        </svg>
                     </div>
-
-                    <div>
-
-                        <Proyect/>
-                        <Proyect/>
-                        <Proyect/>
-
-                    </div>
-
                 </div>
+                <div className="h-1 w-full bg-gray-400 rounded-2xl my-1"></div>
 
-                <div className="bg-gray-300 row-span-3 col-span-1 p-5 rounded-tl-3xl rounded-bl-3xl">
-                    <h1>Miembros</h1>
-                    <div className="h-1 w-full bg-gray-400 rounded-2xl my-1"></div>
+                <Button isActive={
+                        activeButton === 1
+                    }
+                    onClick={
+                        () => handleButtonClick(1)
+                    }/>
+                <Button isActive={
+                        activeButton === 2
+                    }
+                    onClick={
+                        () => handleButtonClick(2)
+                    }/>
+                <Button isActive={
+                        activeButton === 3
+                    }
+                    onClick={
+                        () => handleButtonClick(3)
+                    }/>
 
-                    <Member/>
-                    <Member/>
-                    <Member/>
-
-                </div>
             </div>
+
+            <div className="bg-gray-300 row-span-3 col-span-3 p-5">
+                <div className="h-20 w-auto rounded-3xl bg-gray-500 text-center grid content-center">
+                    <h1 className="text-3xl">Proyecto X</h1>
+                </div>
+
+                <div>
+
+                    <Task/>
+                    <Task/>
+                    <Task/>
+
+                </div>
+
+            </div>
+
+            <div className="bg-gray-300 row-span-3 col-span-1 p-5 rounded-tl-3xl rounded-bl-3xl">
+                <h1>Miembros</h1>
+                <div className="h-1 w-full bg-gray-400 rounded-2xl my-1"></div>
+
+                <Member/>
+                <Member/>
+                <Member/>
+
+            </div>
+
+            <ProyectForm visible={visible} setVisible={setVisible}/>
+        </div>
 
     </>;
 };
