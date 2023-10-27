3266b43dadd011ee74c05bfeebe1546d016e5a21
ale94 <alejandrorua111@gmail.com>
2023-10-26 22:37:06 -0300

    Update controllers

--- Backend/webApp/src/main/java/com/noCountry/webApp/controllers/TaskController.java
+++ Backend/webApp/src/main/java/com/noCountry/webApp/controllers/TaskController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(originPatterns = "*")
 public class TaskController {
 
     private final TaskService taskService;
